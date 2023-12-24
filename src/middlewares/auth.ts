cd5f8178e239077025f3ca72cca81e1044ffea5a
Yurii Baryliak <yuriibaryliak@Yuriis-MacBook-Pro.local>
2023-12-24 21:44:22 +0200

    Add password login

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,5 +1,7 @@
 import { Request, Response, NextFunction } from "express";
+import bcrypt from "bcrypt";
 import CommonProfile from "@/models/CommonProfile";
+import { LOGIN_SCHEMA } from "@/validators/auth";
 
 export const validatePasswordSignUp = async (
   req: Request,
@@ -14,5 +16,49 @@ export const validatePasswordSignUp = async (
     next();
   } catch (e) {
     res.status(400).send(e);
+  }
+};
+
+export const validateLogin = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    await LOGIN_SCHEMA.validate(req.body);
+    next();
+  } catch (e) {
+    res.status(400).send(e);
+  }
+};
+
+export const validateUserCredentials = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const profile = await CommonProfile.findOne({ email: req.body.email });
+    if (!profile) {
+      res.statusCode = 400;
+      next("No user found with such credentials");
+    }
+    if (profile?.password) {
+      const passwordMatches = bcrypt.compareSync(
+        req.body.password,
+        profile.password,
+      );
+      if (!passwordMatches) {
+        res.statusCode = 400;
+        next("No user found with such credentials");
+      }
+    }
+    res.locals = {
+      ...res.locals,
+      profile,
+    };
+    next();
+  } catch (e) {
+    res.send(e);
   }
 };
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -3,7 +3,11 @@ import { omit } from "lodash";
 
 import CommonProfile from "@/models/CommonProfile";
 import { signJWToken } from "@/utils/auth";
-import { validatePasswordSignUp } from "@/middlewares/auth";
+import {
+  validatePasswordSignUp,
+  validateLogin,
+  validateUserCredentials,
+} from "@/middlewares/auth";
 import { COMMON_SERVER_ERROR } from "@/constants/server";
 
 const AuthRouter = Router();
@@ -21,5 +25,23 @@ AuthRouter.post("/signUp", validatePasswordSignUp, async (req, res) => {
     res.status(500).send(COMMON_SERVER_ERROR);
   }
 });
+
+AuthRouter.post(
+  "/login",
+  validateLogin,
+  validateUserCredentials,
+  async (req, res) => {
+    try {
+      const { profile } = res.locals;
+      const token = signJWToken(profile.id);
+      res.status(200).send({
+        token,
+        profile: omit(profile.toObject(), "password"),
+      });
+    } catch (e) {
+      res.status(500).send(COMMON_SERVER_ERROR);
+    }
+  },
+);
 
 export default AuthRouter;
