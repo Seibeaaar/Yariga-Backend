4d935cef62217f2e028dde535b1ab6a7c54fde5e
Yurii Baryliak <yuriibaryliak@Yuriis-MacBook-Pro.local>
2024-01-04 20:40:36 +0200

    Fix cors policy

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import helmet from "helmet";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ import SalesRouter from "./routes/sales";
 const app = express();
 
 app.use(helmet());
+app.use(cors());
 app.use(express.json());
 
 app.use("/auth", AuthRouter);
