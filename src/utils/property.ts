5b6826240b478183ec3e75770f830c2cabd5cbcb
Yurii Baryliak <yuriibaryliak@Yuriis-MacBook-Pro.local>
2024-04-18 21:44:46 +0300

    Get client preferences

--- src/utils/property.ts
+++ src/utils/property.ts
@@ -1,4 +1,14 @@
 import Property from "@/models/Property";
+import { PropertyFilters } from "@/types/property";
+import {
+  FLOOR_LIMIT,
+  AREA_LIMIT,
+  PRICE_LIMIT,
+  BED_LIMIT,
+  ROOM_LIMIT,
+} from "@/enums/property";
+import { PROPERTY_STATUSES } from "@/constants/property";
+import { AGREEMENT_TYPES } from "@/constants/agreement";
 
 export const checkIfPropertyExists = async (id?: string) => {
   if (!id) {
@@ -12,4 +22,93 @@ export const checkIfPropertyExists = async (id?: string) => {
   }
 
   return existingProperty;
+};
+
+export const getPropertyRecommendations = async (
+  filters: PropertyFilters,
+  landlords: string[],
+) => {
+  const queryByFilters = generatePropertyFilterQuery(filters);
+  const results = await Property.aggregate([
+    {
+      $facet: {
+        landlordsProperties: [
+          {
+            $match: {
+              owner: { $in: landlords },
+              ...queryByFilters,
+            },
+          },
+          {
+            $addFields: { sortBy: 0 },
+          },
+        ],
+        others: [
+          {
+            $match: {
+              owner: { $nin: landlords },
+              ...queryByFilters,
+            },
+          },
+          { $sort: { sortBy: -1 } },
+        ],
+      },
+    },
+    {
+      $project: {
+        properties: { $concatArrays: ["$landlordsProperties", "$others"] },
+      },
+    },
+    {
+      $unwind: "$properties",
+    },
+    {
+      $replaceRoot: { newRoot: "$properties" },
+    },
+    {
+      $limit: 500,
+    },
+  ]);
+
+  return results;
+};
+
+export const generatePropertyFilterQuery = (filters: PropertyFilters) => {
+  return {
+    price: {
+      $gte: filters.bottomPrice ?? PRICE_LIMIT.Min,
+      $lte: filters.topPrice ?? PRICE_LIMIT.Max,
+    },
+    area: {
+      $gte: filters.bottomArea ?? AREA_LIMIT.Min,
+      $lte: filters.topArea ?? AREA_LIMIT.Max,
+    },
+    floorLevel: {
+      $gte: filters.bottomFloorLevel ?? FLOOR_LIMIT.Min,
+      $lte: filters.topFloorLevel ?? FLOOR_LIMIT.Max,
+    },
+    beds: {
+      $gte: filters.bottomBedsNumber ?? BED_LIMIT.Min,
+      $lte: filters.topBedsNumber ?? BED_LIMIT.Max,
+    },
+    rooms: {
+      $gte: filters.bottomRoomsNumber ?? ROOM_LIMIT.Min,
+      $lte: filters.topRoomsNumber ?? ROOM_LIMIT.Max,
+    },
+    floors: {
+      $gte: filters.bottomFloorsNumber ?? FLOOR_LIMIT.Min,
+      $lte: filters.topFloorsNumber ?? FLOOR_LIMIT.Max,
+    },
+    agreementType: {
+      $in: filters.agreementType ?? AGREEMENT_TYPES,
+    },
+    ...(filters.facilities && {
+      facilities: {
+        $in: filters.facilities,
+      },
+    }),
+    status: {
+      $in: filters.status ?? PROPERTY_STATUSES,
+    },
+  };
 };
